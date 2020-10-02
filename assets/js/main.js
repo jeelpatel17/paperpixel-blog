be494b4d572793b9b769665833de1ba66e5618b6
Jeel Patel <jeelsardar77@gmail.com>
2020-10-02 17:07:39 +0530

    Version 2.0 Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+AOS.init({
+    duration: 700
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70vw";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
